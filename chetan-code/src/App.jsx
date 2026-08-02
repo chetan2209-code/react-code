543ac4357d8d17fecdfcd55ac8cd63ba7f32c7fa
chetanRana <chetanr2207@gmail.com>
2026-08-02 19:11:58 +0530

    chetan-code done

---
+++ chetan-code/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>try </h1>
+      <h2>cry </h2>
+      <h3>give up! </h3>
+    </div>
+  )
+}
+
+export default App
