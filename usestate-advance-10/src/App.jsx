6f3f479b995c2eea0239dfce0bee8c5eacda6443
chetanRana <chetanr2207@gmail.com>
2026-08-15 12:35:38 +0530

    Usestate-Advanced

---
+++ usestate-advance-10/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [num, setnum] = React.useState({user: 'Chetan', age:19});
+  
+
+  const btnClicked = ()=>{
+    const newNum = {...num};
+    newNum.user = 'Ishwari'
+    setnum(newNum)
+  }
+  
+  return (
+    <div>
+      <h1>{num.user}, {num.age}</h1>
+      <button onClick={btnClicked}>click</button>
+    </div>
+  )
+}
+
+export default App
